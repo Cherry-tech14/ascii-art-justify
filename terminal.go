9dc7273720ef69a55f824481374d2fa81d31ac85
Mariam Mohammed <mohammedmariam0820@gmail.com>
2026-06-04 22:08:17 +0100

    Change GetTerminalWidth to use stty for width
    Refactor GetTerminalWidth to use 'stty' command for terminal size.

--- terminal.go
+++ terminal.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
 )
 
-func GetTerminalWidth() int {
-	out, err := exec.Command("tput", "cols").Output()
+func getTerminalWidth() int {
+	cmd := exec.Command("stty", "size")
+	cmd.Stdin = os.Stdin
+	out, err := cmd.Output()
 	if err != nil {
 		return 80
 	}
-	width, err := strconv.Atoi(strings.TrimSpace(string(out)))
-	if err != nil {
-		return 80
+	parts := strings.Fields(string(out))
+	if len(parts) >= 2 {
+		width, err := strconv.Atoi(parts[1])
+		if err == nil {
+			return width
+		}
 	}
-	return width
+	return 80
 }
